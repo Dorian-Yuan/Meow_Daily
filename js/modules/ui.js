3fc7dc4c3ea7eab9e849b01b8d3faa4587826908
Dorian <doriany@163.com>
2026-03-09 00:06:23 +0800

    chore: bump version to V2.0.23

--- js/modules/ui.js
+++ js/modules/ui.js
@@ -740,7 +740,7 @@ function renderSettings() {
             </div>
             
             <div style="text-align:center; padding:20px;">
-                <p style="font-size:11px; color:var(--color-text-hint); font-weight:600;">Meow_Daily V2.0.22 "SuiSui" Premium Build</p>
+                <p style="font-size:11px; color:var(--color-text-hint); font-weight:600;">Meow_Daily V2.0.23 "SuiSui" Premium Build</p>
             </div>
         </div>
     `;
